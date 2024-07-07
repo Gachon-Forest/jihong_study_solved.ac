865cca7823f31cc6baaba90f5024d614d7386699
jihong <jihong9412@gachon.ac.kr>
2024-07-08 00:42:59 +0900

    p1978 소수찾기 성공

--- solved_study/solved_nodejs/p1978_소수찾기/app.js
+++ solved_study/solved_nodejs/p1978_소수찾기/app.js
@@ -15,3 +15,29 @@ output_ex
 3
 */
 
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => line.split(" ").map(Number));
+
+const N = input[0][0];
+const sosu = input[1];
+
+function solution(N, sosu){
+    let sosu_sum = 0;
+    for(let i = 0; i< N ; i++){
+        let is_sosu = true;
+        for(let j =2; j <= Math.sqrt(sosu[i]); j++){
+            if (sosu[i] % j === 0){//나머지가 0이면 나누어떨어진것이기에 소수 아님
+                is_sosu = false;
+                break;
+            }
+        }
+        if (is_sosu && sosu[i] !== 1) {
+            sosu_sum++;
+        }
+    }
+    return sosu_sum;
+}
+
+const result = solution(N, sosu);
+console.log(result);
